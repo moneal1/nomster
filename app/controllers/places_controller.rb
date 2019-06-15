7d8f840c9abb5375885c3d5feb4c61cde46b9f3e
Mike O'Neal <MikeONeal18@gmail.com>
2019-06-15 02:46:54 +0000

    message about nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -35,6 +35,14 @@ class PlacesController < ApplicationController
 
  ends
 
+ def update
+
+  @place = Place.find(params[:id])
+  @place.update_attributes(place_params)
+  redirect_to root_path
+
+ end
+
   private
 
   def place_params
