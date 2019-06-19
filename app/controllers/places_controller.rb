4a53a03753e4287ff499ec51ea5f1e1cf28bc39b
Mike O'Neal <MikeONeal18@gmail.com>
2019-06-19 03:08:54 +0000

    a message about nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 end
 
@@ -60,8 +60,8 @@ end
  def destroy
 
   @place = Place.find(params[:id])
-  @place.destroy
-  redirect_to root_path
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
 
  end
 
