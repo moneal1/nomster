288c69ab9467c51942b97ac30fdd14d47812990a
Mike O'Neal <MikeONeal18@gmail.com>
2019-06-19 03:24:01 +0000

    a message about nomster

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,9 +19,17 @@ end
 
   def create
 
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
 
-    redirect_to root_path
+    if @place.valid?
+
+      redirect_to root_path
+
+    else
+
+      render :new, status: :unprocessable_entity
+
+    end
 
   end
 
@@ -52,8 +60,22 @@ end
  def update
 
   @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+
+  end
+
   @place.update_attributes(place_params)
-  redirect_to root_path
+
+  if @place.valid?
+    redirect_to root_path
+
+  else
+
+    render :edit, status: :unprocessable_entity
+
+  end
 
  end
 
