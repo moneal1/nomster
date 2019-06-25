d01c401d6768d1869398d6afc6f066525fe54f39
Mike O'Neal <MikeONeal18@gmail.com>
2019-06-25 02:57:00 +0000

    a message about nomster

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'places#index'
+  resources :places do
+    resources :comments, only: :create
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places
 end
